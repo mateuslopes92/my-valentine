8042aedc477fafd0b0c10f22b6bdbec46518f9b1
mateuslopes92 <mateuslopes92@gmail.com>
2025-06-12 17:20:58 -0300

    Add password

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './App.css';
 
+import { useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import TimeCounter from "./Timer";
 import image1 from "./assets/image1.JPG";
@@ -9,49 +10,76 @@ import video1 from "./assets/video1.mp4";
 import video3 from "./assets/video3.mp4";
 
 function App() {
-  const namorando = "2025-06-07T00:00:00"
-  const queNosFalamos = "2025-03-16T00:00:00"
-  const beijo = "2025-03-22T00:00:00"
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [inputPassword, setInputPassword] = useState('');
+  const namorando = "2025-06-07T00:00:00";
+  const queNosFalamos = "2025-03-16T00:00:00";
+  const beijo = "2025-03-22T00:00:00";
+  const password = "velhote";
+
+  const handleLogin = () => {
+    if (inputPassword.toLocaleLowerCase() === password.toLocaleLowerCase()) {
+      setIsAuthenticated(true);
+    } else {
+      alert("Nao vai rolar!");
+    }
+  };
+
+  if (!isAuthenticated) {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '20vh' }}>
+        <h2>Voce e minha novinha e eu sou seu:</h2>
+        <input
+          type="password"
+          value={inputPassword}
+          onChange={e => setInputPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>Ok</button>
+      </div>
+    );
+  }
 
   return (
     <div className='App'>
       <div  style={{ height: 100 }} />
-      <div className="Carousel">
-        <Carousel
-          showThumbs={false}
-          autoPlay={false}
-          infiniteLoop={true}
-          showStatus={false}
-          dynamicHeight
-        >
-          <div>
-            <img height={500}  src={image6} alt="Slide 6" />
-          </div>
-          <div>
-            <video controls width="100%">
-              <source src={video3} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </div>
-          <div>
-            <img height={500}  src={image1} alt="Slide 1" />
-          </div>
-          <div>
-            <video controls width="100%">
-              <source src={video1} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </div>
-        </Carousel>
+      <div className="Polaroid">
+        <div className="Carousel">
+          <Carousel
+            showThumbs={false}
+            autoPlay={false}
+            infiniteLoop={true}
+            showStatus={false}
+            dynamicHeight
+          >
+            <div>
+              <img height={500}  src={image6} alt="Slide 6" />
+            </div>
+            <div>
+              <video controls width="100%">
+                <source src={video3} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+            <div>
+              <img height={500}  src={image1} alt="Slide 1" />
+            </div>
+            <div>
+              <video controls width="100%">
+                <source src={video1} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </Carousel>
+        </div>
       </div>
-      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: 20 }}>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: -60 }}>
         <TimeCounter label="Juntos há:" targetDate={namorando} />
         <TimeCounter label="Nos falamos há:" targetDate={queNosFalamos} />
         <TimeCounter label="Nosso primeiro beijo foi há:" targetDate={beijo} />
         <div style={{ backgroundColor: 'white', width: 200, height: 2, marginTop: 20  }} />
       </div>
 
-      <div style={{padding: 20}}>
+      <div style={{padding: 20, marginBottom: 20}}>
         <div
           style={{
             fontSize: 14,
