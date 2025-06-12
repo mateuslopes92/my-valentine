2b42398ace4eb7707836a005f8e206d8a5a0fa88
mateuslopes92 <mateuslopes92@gmail.com>
2025-06-12 01:44:38 -0300

    Enable PWA with service worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import './index.css';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+serviceWorkerRegistration.register();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
